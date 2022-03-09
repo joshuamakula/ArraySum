f25517169d126483a6b42f675e3df48a4fb50867
Joshua Makula <makulajosh@gmail.com>
2022-03-09 17:48:52 +0300

    sum of arrays

---
+++ Giraffe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 0 } };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for(int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    sum += numbers[i, j];
+                }
+                
+
+            }
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
